0ae0b03bb9f11bef39b0910b7cb808b0cea69964
AliZia3 <alizia0217@gmail.com>
2023-02-07 20:24:35 -0500

    average color of segments

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -21,19 +21,19 @@ public class DotGen {
     public Mesh generate() {
         Set<Vertex> vertices = new HashSet<>();
         // Create all the vertices
-        for(int x = 0; x < width; x += square_size) {
-            for(int y = 0; y < height; y += square_size) {
+        for (int x = 0; x < width; x += square_size) {
+            for (int y = 0; y < height; y += square_size) {
                 vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y).build());
-                vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y).build());
-                vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y+square_size).build());
-                vertices.add(Vertex.newBuilder().setX((double) x+square_size).setY((double) y+square_size).build());
-                
+                vertices.add(Vertex.newBuilder().setX((double) x + square_size).setY((double) y).build());
+                vertices.add(Vertex.newBuilder().setX((double) x).setY((double) y + square_size).build());
+                vertices.add(Vertex.newBuilder().setX((double) x + square_size).setY((double) y + square_size).build());
+
             }
         }
         // Distribute colors randomly. Vertices are immutable, need to enrich them
         Set<Vertex> verticesWithColors = new HashSet<>();
         Random bag = new Random();
-        for(Vertex v: vertices){
+        for (Vertex v : vertices) {
             int red = bag.nextInt(255);
             int green = bag.nextInt(255);
             int blue = bag.nextInt(255);
@@ -44,25 +44,25 @@ public class DotGen {
         }
 
         // Create all the edges
-        
         Set<Segment> edges = new HashSet<>();
-        for(int i =0; i<vertices.size()-1; i++){
-            edges.add(Segment.newBuilder().setV1Idx(i).setV2Idx(i+1).build());
+        for (int i = 0; i < vertices.size() - 1; i++) {
+            edges.add(Segment.newBuilder().setV1Idx(i).setV2Idx(i + 1).build());
         }
 
+        // Distribute the average of each color randomly
         Set<Segment> edgesWithColors = new HashSet<>();
-        for(Segment e: edges){
+        for (Segment e : edges) {
             int red = bag.nextInt(255);
             int green = bag.nextInt(255);
             int blue = bag.nextInt(255);
-            String colorCode = red + "," + green + "," + blue;
+            int avgRed = red / 2;
+            int avgGreen = green / 2;
+            int avgBlue = blue / 2;
+            String colorCode = avgRed + "," + avgGreen + "," + avgBlue;
             Property color = Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
             Segment colored = Segment.newBuilder(e).addProperties(color).build();
             edgesWithColors.add(colored);
         }
-        
-        
-        
 
         return Mesh.newBuilder().addAllVertices(verticesWithColors).addAllSegments(edgesWithColors).build();
     }
--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -10,38 +10,44 @@ import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.util.List;
 
 public class GraphicRenderer {
 
     private static final int THICKNESS = 3;
+
     public void render(Mesh aMesh, Graphics2D canvas) {
         canvas.setColor(Color.BLACK);
         Stroke stroke = new BasicStroke(0.5f);
         canvas.setStroke(stroke);
-        for (Vertex v: aMesh.getVerticesList()) {
-            double centre_x = v.getX() - (THICKNESS/2.0d);
-            double centre_y = v.getY() - (THICKNESS/2.0d);
+        for (Vertex v : aMesh.getVerticesList()) {
+            double centre_x = v.getX() - (THICKNESS / 2.0d);
+            double centre_y = v.getY() - (THICKNESS / 2.0d);
             Color old = canvas.getColor();
             canvas.setColor(extractColor(v.getPropertiesList()));
+
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
             canvas.fill(point);
             canvas.setColor(old);
         }
 
-        for(Segment e: aMesh.getSegmentsList()){
+        for (Segment e : aMesh.getSegmentsList()) {
             int v1Index = e.getV1Idx();
             int v2Index = e.getV2Idx();
             Vertex v1 = aMesh.getVertices(v1Index);
             Vertex v2 = aMesh.getVertices(v2Index);
-            canvas.drawLine((int)v1.getX(), (int)v1.getY(), (int)v2.getX(), (int)v2.getY());
+            Color old = canvas.getColor();
+            canvas.setColor(extractColor(e.getPropertiesList()));
+            canvas.drawLine((int) v1.getX(), (int) v1.getY(), (int) v2.getX(), (int) v2.getY());
+            canvas.setColor(old);
         }
-        
+
     }
 
     private Color extractColor(List<Property> properties) {
         String val = null;
-        for(Property p: properties) {
+        for (Property p : properties) {
             if (p.getKey().equals("rgb_color")) {
                 System.out.println(p.getValue());
                 val = p.getValue();
